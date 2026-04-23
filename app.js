88bf1b522459d1bf63b9d57b81a180a8fdabc53c
unknown <devsyedaliadnan@gmail.com>
2026-04-23 23:58:17 +0500

    frist message

---
+++ app.js
@@ -0,0 +1,112 @@
+//Question 1//
+console.log("HelloWorld");
+console.log("My name is Syed Ali Adnan");
+let myName = "Syed Ali Adnan";
+//let myAge = '28';
+let myCity = "Karachi";
+console.log(myName);
+console.log(myAge);
+console.log(myCity);
+
+
+/*Question 2
+1. JavaScript ka use kis liya hota hai?
+ans. Java script is a programming language. Used to make website dynamic and interactive.
+2. Kiya javascript browser ma run hosakti hai?
+Ans. Yes 
+3. High level language ka matlb kia hai?
+Ans. its means this is closed to human language.
+4.Compiler aur interpreter mai sy javascript kis par kam krti hai?
+Ans. Both use in modern days.
+5. kia javascriptfronent k liya use hoti hai?
+Ans. yes 
+*/
+
+
+//Question3//
+
+let num = '30';
+const myNam = "Ali";
+var myAge ='28';
+
+var aboveResult = "Result Check"
+console.log(aboveResult);
+var aboveResult = "edit result"
+console.log(aboveResult);
+
+//Question 4
+
+var string = "Syed Ali Adnan";
+//var num = '30';
+var bolean = true;
+var empty ="null";
+var nul ="";
+
+
+//question 5
+
+var a = "ali"
+console.log(typeof a);
+
+var b ='3';
+console.log(typeof b);
+
+var c =true;
+console.log(typeof c);
+
+var d = 
+console.log(typeof d);
+
+var e=[];
+console.log(typeof e);
+
+//question 6
+ let addision = 2+2;
+ console.log("Result = "+addision);
+ let subtraction = 5-2;
+ console.log("Result = "+ subtraction);
+ let multiplication = 5*2;
+ console.log("Result = "+ multiplication);
+ let division = 5/2;
+ console.log("Result = "+ division);
+ let reminder = 5%2;
+ console.log("Result = "+ reminder);
+ 
+
+ //question 7
+
+ let comp = 5=="5"
+ console.log(comp);
+ let comp2 = 5==="5"
+ console.log(comp2);
+ let comp3 = 10>5
+ console.log(comp3);
+ let comp4 = 3<1
+ console.log(comp4);
+
+ //question 8
+
+ x =5
+ x++
+console.log(x);
+ y=5
+ ++y
+ console.log(y);
+ 
+ x =5
+ x--
+console.log(x);
+ y=5
+ --y
+ console.log(y);
+
+ //question 9
+
+ num1 = 10;
+ num2 = 15;
+
+ console.log(num1+num2);
+ console.log(num1 -num2);
+ console.log(num1 *num2);
+ console.log(num1 /num2);
+ 
